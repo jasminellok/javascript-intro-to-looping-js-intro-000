dcb600e789a394f4225e5c57032fdfe8443cb2db
Jasmine Lok <jasminellok@gmail.com>
2020-04-09 23:49:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,14 @@ function whileLoop(n) {
   }
   return 'done'
 }
+
+
+function doWhileLoop(num) {
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < num)      
+}
